427c8c7a3cb3a15ed32770508efee1c7a4e88d2a
elda27 <kaz.birdstick@gmail.com>
2017-03-05 20:21:31 +0900

    Fixed to add change directory

--- runServer.py
+++ runServer.py
@@ -4,6 +4,7 @@ import sys
 sys.path.append('./Source/')
 
 import os
+import os.path
 import www.server
 
 import logging
@@ -15,5 +16,7 @@ sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.INFO)
 www.server.app.logger.addHandler(stream_handler)
-os.chdir('./www')
+
+os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'www'))
+
 www.server.main(True)
